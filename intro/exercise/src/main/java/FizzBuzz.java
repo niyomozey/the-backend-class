7df85505d849b3ec1a53ab0712f43d63045d2759
Christian Dräger <christian.draeger@mhp.com>
2022-02-10 14:18:01 +0100

    add soluition for fizzbuzz exercise

--- intro/exercise/src/main/java/FizzBuzz.java
+++ intro/exercise/src/main/java/FizzBuzz.java
@@ -1,5 +1,26 @@
 public class FizzBuzz {
-    String helloWorld() {
-        return "hello";
+
+    public static void main(String[] args) {
+        fizzBuzz(100);
     }
+
+    static void fizzBuzz(int max) {
+        for (int i = 1; i <= max; i++) {
+            System.out.println(calculate(i));
+        }
+    }
+
+    static String calculate(int number) {
+        if (number% 5 == 0 && number % 3 == 0) {
+            return "fizzbuzz";
+        }
+        if(number % 3 == 0) {
+            return "fizz";
+        }
+        if(number % 5 == 0) {
+            return "buzz";
+        }
+        return Integer.toString(number);
+    }
+
 }
--- intro/exercise/src/test/java/FizzBuzzTest.java
+++ intro/exercise/src/test/java/FizzBuzzTest.java
@@ -1,5 +1,7 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.data.Percentage;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
@@ -7,7 +9,56 @@ class FizzBuzzTest {
     FizzBuzz subjectUnderTest = new FizzBuzz();
 
     @Test
-    void helloWorld() {
-        assertThat(subjectUnderTest.helloWorld()).isEqualTo("hello");
+    void shouldReturn1() {
+        String result = subjectUnderTest.calculate(1);
+        assertThat(result).isEqualTo("1");
+    }
+
+
+    @Test
+    void shouldReturn2() {
+        String result = subjectUnderTest.calculate(2);
+        assertThat(result).isEqualTo("2");
+    }
+
+
+    @Test
+    void number3shouldReturnFizz() {
+        String result = subjectUnderTest.calculate(3);
+        assertThat(result).isEqualTo("fizz");
+    }
+
+
+    @Test
+    void number9shouldReturnFizz() {
+        String result = subjectUnderTest.calculate(9);
+        assertThat(result).isEqualTo("fizz");
+    }
+
+
+    @Test
+    void shouldReturn4() {
+        String result = subjectUnderTest.calculate(4);
+        assertThat(result).isEqualTo("4");
+    }
+
+
+    @Test
+    void number5shouldReturnBuzz() {
+        String result = subjectUnderTest.calculate(5);
+        assertThat(result).isEqualTo("buzz");
+    }
+
+    @Test
+    void number10shouldReturnBuzz() {
+        String result = subjectUnderTest.calculate(10);
+        assertThat(result).isEqualTo("buzz");
+    }
+
+
+    @Test
+    void number15shouldReturnFizzBuzz() {
+        String result = subjectUnderTest.calculate(15);
+        assertThat(result).isEqualTo("fizzbuzz");
     }
 }
